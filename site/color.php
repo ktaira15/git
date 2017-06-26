ff411a63aedf85e6fd7f688221055c02620dd478
ktaira15 <ktaira@wustl.edu>
2017-06-26 15:13:43 -0500

    successful colorcalc.php
    operational color calculator

--- site/color.php
+++ site/color.php
@@ -1,19 +1,19 @@
+
+
 <html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="color.css">
-  </head>
   <body>
     <?php
+    #DEFUNCT, REFER TO COLORCALC.PHP
     include('config/init.php');
       //color calculator with two drop down lists with same colors (red, yellow, blue) amd a submit button--when you submit, background of page becomes combined color
-    if(isset($_REQUEST[orange])){
+    if(isset($_REQUEST['orange'])){
       //do i put echo before function here if i define CSS bg color in function itself? or will it do it automatically with how i have it formatted now
       orange($_REQUEST['red'], $_REQUEST['yellow']);
     }
-    elseif(isset($_REQUEST[purple])){
+    elseif(isset($_REQUEST['purple'])){
       purple($_REQUEST['red'], $_REQUEST['blue']);
     }
-    elseif (isset($_REQUEST[green])){
+    elseif (isset($_REQUEST['green'])){
       green($_REQUEST['yellow'], $_REQUEST['blue']);
     }
     else {
@@ -27,7 +27,7 @@
 
     function orange($red, $yellow){
 
-      
+
       #CSS would go here? or in if statements? Unsure if this looks right ..... OR WOULD I HAVE TO DO THIS AS A DIV WITH HTML??? consider
       #Like pretty sure I need a separate CSS stylesheet (or at least I will down the line) and have to integrate it somehow
     }
@@ -64,6 +64,5 @@
       </form>
     ";
     ?>
-
   </body>
 </html>
---
+++ site/colorcalc.php
@@ -0,0 +1,75 @@
+<html>
+  <body>
+    <?php
+      include('config/init.php');
+      $red = '#cc210e';
+      $yellow ='#fffd91';
+      $blue = '#488daf';
+      $orange = '#e0a81a';
+      $purple = '#955da3';
+      $green = '#99b580';
+
+      function AddColors($color1, $color2){
+        global $red;
+        global $yellow;
+        global $blue;
+        global $orange;
+        global $purple;
+        global $green;
+
+        if($color1==$red && $color2==$yellow){
+          $color = $orange;
+        }
+        elseif($color1==$red && $color2==$blue){
+          $color = $purple;
+        }
+        elseif($color1==$yellow && $color2==$blue){
+          $color = $green;
+        }
+        elseif($color1==$color2){
+          $color = $color1;
+        }
+        elseif($color1==$yellow && $color2==$red){
+          $color = $orange;
+        }
+        elseif($color1==$blue && $color2==$red){
+          $color = $purple;
+        }
+        elseif($color1==$blue && $color2==$yellow){
+          $color = $green;
+        }
+        return $color;
+      }
+
+      if(isset($_REQUEST['color1'])){
+        $combo = AddColors($_REQUEST['color1'], $_REQUEST['color2']);
+      }
+      else{
+        $combo = 'white';
+      }
+
+      echo "
+        <style>
+          body {
+            padding: 20%;
+            text-align: center;
+            background-color: $combo;
+          }
+        </style>
+        <form action='' method='get'>
+          <select name='color1' />
+            <option name=$red value=$red /> Red
+            <option name=$yellow value=$yellow /> Yellow
+            <option name='$blue' value='$blue' /> Blue
+          </select>
+          <select name='color2' />
+            <option name=$red value=$red /> Red
+            <option name=$yellow value=$yellow /> Yellow
+            <option name='$blue' value='$blue' /> Blue
+          </select> <br>
+          <input name='submitForm' type='submit' />
+        </form>
+      ";
+    ?>
+  </body>
+</html>
